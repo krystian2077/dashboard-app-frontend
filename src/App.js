df4a004c862cf1affb88b6a475194a41fee87367
Krystian <krystian.potaczek07@gmail.com>
2023-04-09 17:22:23 +0200

    Create Products Page.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
-import { useSelector } from "react-redux";
 import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
 
-import { Layout } from "scenes/layout";
 import { Dashboard } from "scenes/dashboard";
+import { Layout } from "scenes/layout";
+import { Products } from "scenes/products";
 
 const App = () => {
-  const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+	const mode = useSelector((state) => state.global.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-  return (
-    <>
-      <BrowserRouter>
-        <div className="app">
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Routes>
-              <Route element={<Layout />}>
-                <Route
-                  path="/"
-                  element={<Navigate to="/dashboard" replace />}
-                />
-                <Route path="/dashboard" element={<Dashboard />} />
-              </Route>
-            </Routes>
-          </ThemeProvider>
-        </div>
-      </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<div className="app">
+				<BrowserRouter>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						<Routes>
+							<Route element={<Layout />}>
+								<Route
+									path="/"
+									element={<Navigate to="/dashboard" replace />}
+								/>
+								<Route path="/dashboard" element={<Dashboard />} />
+								<Route path="/products" element={<Products />} />
+							</Route>
+						</Routes>
+					</ThemeProvider>
+				</BrowserRouter>
+			</div>
+		</>
+	);
 };
 
 export default App;
