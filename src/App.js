17cb9c1d87d60b9e4142e1a005138a2a119455e1
Krystian <krystian.potaczek07@gmail.com>
2023-04-09 19:22:04 +0200

    Adding Customers Page.

--- src/App.js
+++ src/App.js
@@ -8,32 +8,34 @@ import { themeSettings } from "./theme";
 import { Dashboard } from "scenes/dashboard";
 import { Layout } from "scenes/layout";
 import { Products } from "scenes/products";
+import { Customers } from "scenes/customers";
 
 const App = () => {
-	const mode = useSelector((state) => state.global.mode);
-	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-	return (
-		<>
-			<div className="app">
-				<BrowserRouter>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<Routes>
-							<Route element={<Layout />}>
-								<Route
-									path="/"
-									element={<Navigate to="/dashboard" replace />}
-								/>
-								<Route path="/dashboard" element={<Dashboard />} />
-								<Route path="/products" element={<Products />} />
-							</Route>
-						</Routes>
-					</ThemeProvider>
-				</BrowserRouter>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="app">
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Routes>
+              <Route element={<Layout />}>
+                <Route
+                  path="/"
+                  element={<Navigate to="/dashboard" replace />}
+                />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/customers" element={<Customers />} />
+              </Route>
+            </Routes>
+          </ThemeProvider>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 };
 
 export default App;
