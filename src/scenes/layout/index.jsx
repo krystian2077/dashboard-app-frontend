d4625d3989d8c6ba9ccf21c1bce71459407983e1
Krystian <krystian.potaczek07@gmail.com>
2023-04-25 00:01:24 +0200

    Finishing DashBoard page

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -7,32 +7,28 @@ import { Outlet } from "react-router-dom";
 import { useGetUserQuery } from "state/api";
 
 export const Layout = () => {
-	const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
 
-	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-	const userId = useSelector((state) => state.global.userId);
-	const { data } = useGetUserQuery(userId);
-	console.log("data", data);
-
-	return (
-		<>
-			<Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
-				<Sidebar
-					user={data || {}}
-					isNonMobile={isNonMobile}
-					drawerWidth="300px"
-					isSidebarOpen={isSidebarOpen}
-					setIsSidebarOpen={setIsSidebarOpen}
-				/>
-				<Box flexGrow={1}>
-					<Navbar
-						user={data || {}}
-						isSidebarOpen={isSidebarOpen}
-						setIsSidebarOpen={setIsSidebarOpen}
-					/>
-					<Outlet />
-				</Box>
-			</Box>
-		</>
-	);
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth="300px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
